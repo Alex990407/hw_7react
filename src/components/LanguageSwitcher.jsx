7c114ee278c19527ce1d4ac0be2d5f6fae0d7516
Alex990407 <zhelezniako07@gmail.com>
2024-08-26 23:21:46 +0200

    second commit. Add styles.

--- src/components/LanguageSwitcher.jsx
+++ src/components/LanguageSwitcher.jsx
@@ -2,9 +2,12 @@ import React, { useContext } from "react";
 import { LanguageContext } from "../context/LanguageContext";
 
 const LanguageSwitcher = () => {
-  const { toggleLanguage } = useContext(LanguageContext);
+  const { language, toggleLanguage } = useContext(LanguageContext);
 
-  return <button onClick={toggleLanguage}>Переключить язык</button>;
+  const buttonText =
+    language === "en" ? "Switch to Russian" : "Переключить на английский";
+
+  return <button onClick={toggleLanguage}>{buttonText}</button>;
 };
 
 export default LanguageSwitcher;
